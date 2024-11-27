5acabb31f85e271dd55542a514ab81bae01f952e
zzl-hyun <kimgihyun877@gmail.com>
2024-11-27 14:57:49 +0900

    "init"

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'banananananana' });
+  res.render('index', { title: 'banananananana' ,ip:req.ip});
+  console.log('Client IP:', req.ip);
 });
+
 
 module.exports = router;
