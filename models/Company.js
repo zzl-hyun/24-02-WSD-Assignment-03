66bc1ed1ebcea4b6792f76fc45c82ceed02f50a6
zzl-hyun <kimgihyun877@gmail.com>
2024-12-01 19:31:41 +0900

    "Define Company Schema"

--- models/Company.js
+++ models/Company.js
@@ -13,3 +13,19 @@
 //   }
   
 //   company model 만들기
+
+const mongoose = require('mongoose');
+
+const CompanySchema = new mongoose.Schema({
+  company_name: { type: String, required: true }, // 회사명
+  representative_name: { type: String, required: true }, // 대표자명
+  company_type: { type: String, required: true }, // 기업형태
+  industry: { type: String, required: true }, // 업종
+  employee_count: { type: String }, // 사원수
+  establishment_date: { type: Date }, // 설립일
+  revenue: { type: String }, // 매출액
+  homepage: { type: String }, // 홈페이지
+  company_address: { type: String }, // 기업주소
+});
+
+module.exports = mongoose.model('Company', CompanySchema);
