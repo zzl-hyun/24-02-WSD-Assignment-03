eea3405bdfb6e664fd07163b0faca614cc8a3fa8
zzl-hyun <kimgihyun877@gmail.com>
2024-12-07 15:59:06 +0900

    Add: 지원서 수락/거절

--- src/routes/api/admin.js
+++ src/routes/api/admin.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../../middlewares/authenticateToken');
 const isAdmin = require('../../middlewares/isAdmin');
-const { getApplications } = require('../../controllers/application.controller');
+const { validateID ,validateStatus } = require('../../middlewares/validators');
+const { getApplications, changeStatus } = require('../../controllers/application.controller');
+
 
 /**
  * @swagger
@@ -13,7 +15,7 @@ const { getApplications } = require('../../controllers/application.controller');
 
 /**
  * @swagger
- * /admin/jobs:
+ * /admin/applications:
  *   get:
  *     summary: 지원 내역 조회
  *     tags: [Admin]
@@ -33,6 +35,46 @@ const { getApplications } = require('../../controllers/application.controller');
  *                          items:
  *                              $ref: '#/components/schemas/Application'
  */
-router.get('/jobs', authenticateToken, isAdmin, getApplications);
+router.get('/applications', authenticateToken, isAdmin, getApplications);
 
+/**
+ * @swagger
+ * /admin/applications/{id}:
+ *   put:
+ *     summary: 지원서 수락/거절
+ *     tags: [Admin]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: The ID of the application to update
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: status
+ *         in: query
+ *         description: The new status of the application
+ *         required: true
+ *         schema:
+ *           type: string
+ *           enum:
+ *             - Accepted
+ *             - Rejected
+ *           example: Accepted
+ *     responses:
+ *       200:
+ *         description: Application updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: success
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Application'
+ */
+router.put('/applications/:id', authenticateToken, isAdmin, validateID, validateStatus, changeStatus)
 module.exports = router;
